6851cafaa78bb0c4556dd30b554b0c9748a85687
Nagasai <91328922+Nagsai-Thumati@users.noreply.github.com>
2026-06-02 19:11:40 +0200

    Delete Pytest-Fundamentals-Testing-Framework-of-Python/PytestsDemo directory

--- Pytest-Fundamentals-Testing-Framework-of-Python/PytestsDemo/test_fixturesData.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-
-@pytest.mark.usefixtures("dataload")
-class TestExample2:
-
-    def test_editprofile(self,dataload):
-        print("Name:",dataload[0], dataload[1])
-        print(dataload[2])
